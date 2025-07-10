a87c05112648835e9ef4847b01ce561cca5a85cf
wendwin <wendwin12@gmail.com>
2025-07-10 23:01:50 +0700

    merubah halaman home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,19 +8,19 @@
                 <h5 class="text-sm font-semibold text-slate-500 mb-2"> &commat;faisal.d.n</h5>
                 <h5 class="text-sm font-semibold text-slate-500"><font-awesome-icon :icon="['fas', 'location-dot']" class="text-slate-500" /> Yogyakarta, ID</h5>
                 <h1 class="text-2xl font-semibold text-slate-800 dark:text-white mt-6 md:text-4xl mb-2">Hi, my name is <span class="bg-gradient-to-r from-[#02AEB0] to-[#00C9AC] bg-clip-text text-transparent">Faisal DN</span></h1>
-                <h1 class="text-2xl font-semibold  md:text-4xl text-slate-800 dark:text-white">I'm Fullstack Developer</h1>
+                <h1 class="text-2xl font-semibold  md:text-4xl text-slate-800 dark:text-white">I'm Fullstack Web Developer</h1>
             </div>
             <div class="my-10 flex flex-wrap justify-center items-center gap-10 text-2xl text-slate-500 ">
                 <a href="https://www.instagram.com/faisal.d.n" target="_blank">
                     <font-awesome-icon :icon="['fab', 'instagram']" class="hover:text-[#02AEB0] hover:scale-125 transition ease-in duration-100 " />
                 </a>    
-                <a href="" target="_blank">
+                <a href="#" target="_blank">
                     <font-awesome-icon :icon="['fab', 'discord']" class="hover:text-[#02AEB0] hover:scale-125 transition ease-in duration-100 " />
                 </a>
                 <a href="https://github.com/wendwin" target="_blank">
                     <font-awesome-icon :icon="['fab', 'github']" class="hover:text-[#02AEB0] hover:scale-125 transition ease-in duration-100 "  />
                 </a>
-                <a href="" target="_blank">
+                <a href="https://www.linkedin.com/in/faisal-dwi-nurwenda-87a625373" target="_blank">
                     <font-awesome-icon :icon="['fab', 'linkedin-in']" class="hover:text-[#02AEB0] hover:scale-125 transition ease-in duration-100 " />
                 </a>
                 <a href="mailto:faisaldwindn@gmail.com" target="_blank">
